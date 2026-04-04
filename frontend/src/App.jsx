79c4875c078ea5946c84bdd6b481f0478a6de2d2
Samia-i <inaksamia@gmail.com>
2026-04-04 21:18:24 +0100

    feat: configuration du routage et affichage du composant Dashboard #19

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; // <-- CETTE LIGNE MANQUE
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
