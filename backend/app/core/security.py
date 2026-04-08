757d9a7880dcc23eeba770ae7cfe5d40b19a0674
IMANE-10 <imaneighrane@gmail.com>
2026-04-08 19:36:11 +0200

    🔵 Blue Team T10 — Sécurité MySQL, Hashage & Gestion des accès

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -4,10 +4,17 @@ from fastapi import Depends, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 import os
 from dotenv import load_dotenv
+from passlib.context import CryptContext
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "secret")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+#clé secrete 
+SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY manquante dans .env — arrêt de l'application")
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
@@ -16,13 +23,13 @@ bearer_scheme = HTTPBearer(
     description="Paste a bearer token obtained from POST /api/auth/login.",
 )
 
-
+#hacher le password
 def hash_password(password: str) -> str:
-    return password
+    return pwd_context.hash(password)
 
-
+#comprer le pasword saisi avec le hash en BD
 def verify_password(plain_password: str, stored_password: str) -> bool:
-    return plain_password == stored_password
+    return pwd_context.verify(plain_password, stored_password)
 
 
 def create_access_token(data: dict):
@@ -45,3 +52,18 @@ def get_current_user(
 
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
+
+#Verifier le role 
+def require_role(allowed_roles: List[str]):
+
+    def role_checker(
+        current_user: dict = Depends(get_current_user)
+    ):
+        user_role = current_user.get("role", "")
+        if user_role.lower() not in [r.lower() for r in allowed_roles]:
+            raise HTTPException(
+                status_code=403,
+                detail=f"Accès refusé — Rôle requis : {allowed_roles}"
+            )
+        return current_user
+    return role_checker
